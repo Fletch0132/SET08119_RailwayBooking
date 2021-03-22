d1e342cf4d805ed72d5291c991393e4575018a9d
Fletcher Moore <42897038+Fletch0132@users.noreply.github.com>
2021-03-22 13:49:31 +0000

    Files uploaded, coursework complete 2018

---
+++ RailwayPlanningSystem/BusinessLayer/Booking.cs
@@ -0,0 +1,137 @@
+﻿//Author: Fletcher Thomas Moore
+/*Description: Retrieves the user/customer information from the presentation layer and stores it within the data layer as well as 
+               displaying the information from the data layer within the presentation layer*/
+//Start Date: 19/11/2018
+//Last Edit: 04/01/2019
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Booking
+    {
+        //setting up variables
+        private string _bookingFirstName, _bookingLastName, _bookingTrainID, _bookingDepartStation, _bookingArrivalStation, _bookingCoach, _bookingSeat;
+        private bool _bookingFirstClass, _bookingCabin;
+
+        //get-set fistName
+        public string firstName
+        {
+            get
+            {
+                return _bookingFirstName;
+            }
+            set
+            {
+                _bookingFirstName = value;
+            }
+        }
+
+        //get-set lastName
+        public string lastName
+        {
+            get
+            {
+                return _bookingLastName;
+            }
+            set
+            {
+                _bookingLastName = value;
+            }
+        }
+
+        //get-set booking TrainID
+        public string trainID
+        {
+            get
+            {
+                return _bookingTrainID;
+            }
+            set
+            {
+                _bookingTrainID = value;
+            }
+        }
+
+        //get-set departStation
+        public string departStation
+        {
+            get
+            {
+                return _bookingDepartStation;
+            }
+            set
+            {
+                _bookingDepartStation = value;
+            }
+        }
+
+        //get-set arrivalStation
+        public string arrivalStation
+        {
+            get
+            {
+                return _bookingArrivalStation;
+            }
+            set
+            {
+                _bookingArrivalStation = value;
+            }
+        }
+
+        //get-set coach
+        public string coach
+        {
+            get
+            {
+                return _bookingCoach;
+            }
+            set
+            {
+                _bookingCoach = value;
+            }
+        }
+
+        //get-set seat
+        public string seat
+        {
+            get
+            {
+                return _bookingSeat;
+            }
+            set
+            {
+                _bookingSeat = value;
+            }
+        }
+
+        //get-set firstClass
+        public bool bookFirstClass
+        {
+            get
+            {
+                return _bookingFirstClass;
+            }
+            set
+            {
+                _bookingFirstClass = value;
+            }
+        }
+
+        //get-set cabin
+        public bool cabin
+        {
+            get
+            {
+                return _bookingCabin;
+            }
+            set
+            {
+                _bookingCabin = value;
+            }
+        }
+    }
+}
---
+++ RailwayPlanningSystem/BusinessLayer/FarePrices.cs
@@ -0,0 +1,83 @@
+﻿//Author: Fletcher Thomas Moore
+/*Description: Stores the prices as private for the fares, can be accessed through get-set to calculate full price*/
+//Start Date: 19/11/2018
+//Last Edit: 13/12/2018
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class FarePrices
+    {
+        //Set fares
+        private decimal _FareEdinLon = 50.00M, _FareInterStation = 25.00M, _FareFirstClass = 10.00M, _FareSleeper = 10.00M, _FareSleeperCabin = 20.00M;
+
+        //Fare price from Edinburgh to London (Vice versa)
+        public decimal fareEdinLon
+        {
+            get
+            {
+                return _FareEdinLon;
+            }
+            set
+            {
+                _FareEdinLon = value;
+            }
+        }
+
+        //Fare price from two intermediate stations or Edin/Lon to intermediate (vice versa)
+        public decimal fareInterStation
+        {
+            get
+            {
+                return _FareInterStation;
+            }
+            set
+            {
+                _FareInterStation = value;
+            }
+        }
+
+        //Fare Surcharge for first class
+        public decimal fareFirstClass
+        {
+            get
+            {
+                return _FareFirstClass;
+            }
+            set
+            {
+                _FareFirstClass = value;
+            }
+        }
+
+        //Fare surcharge for Sleeper
+        public decimal fareSleeper
+        {
+            get
+            {
+                return _FareSleeper;
+            }
+            set
+            {
+                _FareSleeper = value;
+            }
+        }
+
+        //Fare surcharge for Sleeper Cabin
+        public decimal fareSleeperCabin
+        {
+            get
+            {
+                return _FareSleeperCabin;
+            }
+            set
+            {
+                _FareSleeperCabin = value;
+            }
+        }
+    }
+}
